1d9247272875a1a5d93dacd029c817bf78aa01f3
Evan <djevann11@gmail.com>
2021-04-12 08:03:10 +0700

    adding new image

--- detection.py
+++ detection.py
@@ -8,6 +8,7 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
 detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+# detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "gambar.jpg"), output_image_path=os.path.join(execution_path , "gambarnew.jpg"))
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
